769196fc9b7b74fd288f57aeedf5482d060a0046
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-12-20 08:52:55 +0000

    conversion of Operations

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,34 @@
+let score = "33abc"         
+// when we put 33abc and we try to convert it in number it will be converted but when we check the actual valueinNUmber it is NaN(not a number) also same in undefined case
+//note ; when we put null in score it will convert it in num which is 0
+// console.log(typeof score)
+// console.log(typeof(score))
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN (but iska type number hi he)
+// null => 0 
+//true = 1;false =0;
+
+// let convert number in to boolean
+let isloggedIn =1;
+let numtoBoolean = Boolean(isloggedIn)
+// console.log(numtoBoolean)
+// console.log(typeof numtoBoolean)
+
+// 1 => true
+//0 => false
+// "" => false
+// "hitesh" => true
+
+
+//now num to string 
+let someNum = 33
+let someNumToString = String(someNum)
+// console.log(someNumToString)
+// console.log(typeof someNumToString)
+
+// +++++++++++++++++++++++++++++++Operations+++++++++++++++++++++++++++++++++++++++
