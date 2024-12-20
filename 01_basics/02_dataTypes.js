44561eee99d6168d830a8e9f36fb36cc8d69a43b
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-12-20 07:42:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treats all over code as a newer version of JS
+// alert (3 + 3); //we are using nodeJs, not browser
+
+let nmae ="Dhanesh"
+let age =18
+let isloggedIn = true
+let state = null
+
+
+
+//types of datetypes
+//number => 2  to power 53
+//string =""
+//boolean => true/ false
+//null => standalone value
+//undefined =>
+//symbol => unique
+ // object
+
+
+// we can  check the type of data by using 
+console.log(typeof "Hitesh")
+//or 
+console.log(typeof age)
+// we get out string and number output respectively
+console.log(typeof null)  // we get object output bcz null is an object
+console.log(typeof undefined)// we get undefined as a output
+
