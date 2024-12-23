73d4ff71f642a36d99dad241c80a3b6960ebd1ec
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-12-23 14:08:26 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log(null > 0);  //output false
+// console.log(null ==0);  //output false
+// console.log(null <0);   //output false
+// console.log(null>=0);   //output true
+
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined === 0);
+// console.log(undefined < 0);
+
+//note ; The above conversions are not practical in real project or in industries
+
+// we  use === which is strict check , checks also data type 
+
+console.log(2 === 2);
+
+
+
+
+
+
+
+
+
+
+
