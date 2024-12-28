05e7370880ee60c889e939e9420f5faaa25d8f78
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2024-12-28 06:55:20 +0000

    math and num

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,50 @@
+const score = 400;
+const balance = new Number(400)
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));  // point k bad k digits
+
+const othernum = 123.89637
+// console.log(othernum.toPrecision(3)); 
+// total digits kitne hone chaiye
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))  // we get 10,00,000
+
+
+
+
+//++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.abs(-4));  //-ve value ko positive kerta he
+// console.log(Math.round(3.566632));  //round-off
+// console.log(Math.floor(4.999));// we get 4 
+// console.log(Math.ceil(4.0001)); // we get 5
+// console.log(Math.min(3,1,5,9,2,0)); //we get minimum value
+// console.log(Math.max(2,5,7,5,8,9));// we get maximum value
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10))+1;
+
+const min = 10;
+const max =20;
+console.log(Math.floor(Math.random()*(max -min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
