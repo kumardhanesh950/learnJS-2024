57de4eb2355305afb8e83d8d18e538011c75a7fa
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2025-01-03 08:47:16 +0000

    object_02

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,58 @@
+const tinderUser = new Object()   
+// console.log(tinderUser)  //output =>  {}  but this is singleton object 
+const  tinder_user ={}
+// console.log(tinder_user)   //output =>  {} this is non-singleton object
+
+tinderUser.id ="123abc"
+tinderUser.name ="sammy"
+tinderUser.isLoggedIn =false
+// console.log(tinderUser)
+
+//note hum object k ander object or object k under object declare or access ker sakte as below:
+const regularUser ={  
+    email : "some@gmail.com",
+    fullname :{
+        userFullname :{
+            firstName: "Dhanesh",
+            lastName :"Kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullname.firstName);
+const obj1 = {1 : "a", 2:"b"};
+const obj2 ={3: "a", 4: "b"}
+// const obj3 =Object.assign({}, obj1, obj2)   // empty object is me target hota he or variable is me source
+//OR we use mostly sprread operator
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+
+const users =[
+    {
+        id :1,
+        email:"h@gmail.com"
+    },
+    {
+        id :2,
+        email:"h2@gmail.com"
+    },
+    {
+        id :3,
+        email:"h3@gmail.com"
+    }
+]
+// console.log(users[1].email);
+// console.log(users[2].id); 
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('name'))
+
+
+
+
+
+
+
