799847ac9954a30f72993d71659797edaeb5c267
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2025-01-26 06:18:45 +0000

    while & do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+// while (condition) {
+    
+// }
+let x =1;
+
+while (x<=10) {
+    // console.log(`value of x : ${x}`)
+    x = x*2
+}
+
+
+let Myarray = [ "flash", "batman", "superman"]
+let Arr = 0 
+while (Arr <Myarray.length) {
+    // console.log(`Array is ${Myarray[Arr]}`)
+    Arr = Arr+1
+}
+
+// let score = 1
+// do {
+//     console.log(` score is ${score}`)
+//     score++
+// } while (score <= 10);
+
+// observe here we have score =11  phir bi loop ek value deerha ku do while loop me kam pehly hota he isliye pehly 11 print hoga
+let score = 11
+do {console.log(` score is ${score}`)
+    score++
+} while (score <= 10);
