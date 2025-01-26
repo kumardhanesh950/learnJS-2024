0b260b582556393bb5150d6d0b1dd975a3d9a3c5
kumardhanesh950 <65038899+kumardhanesh950@users.noreply.github.com>
2025-01-26 14:13:37 +0000

    for_of loop and Map obj

---
+++ 05_iterations/three.js
@@ -0,0 +1,25 @@
+const  Arr =[ 1,2,3,4,5]
+for (const num of Arr) {
+    // console.log(num);
+    
+}
+
+const greeting = "Hello World"
+for (const greet of greeting) {
+    
+    // console.log(`value of greet is ${greet}`) ;
+    
+    
+}
+
+// Maps
+const map = new Map()
+map.set('IND', "India")
+map.set('USA', "United Nations of America")
+map.set('Fr', "France")
+// console.log(map)
+
+ for (const [ key ,value] of map) {
+    console.log(key, ":-", value);
+    
+ }
